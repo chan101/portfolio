0b04b40bf6cab3fb3fb9368a6e61eb77cc6a1eb8
chan101 <chandankumarchan1999@gmail.com>
2023-10-05 02:11:22 +0530

    move themecontrol to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 //import '@/styles/globals.css'
 import '@/styles/scrollbar.css'
 import { ThemeProvider } from "@mui/material/styles";
-import useMyTheme from "./components/ThemeControl";
 import { CssBaseline, Toolbar } from "@mui/material";
 import Navbar from "./components/Navbar";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
+import { darkTheme, lightTheme } from "@/public/theme";
 
 export default function App({ Component, pageProps }) {
   const { chosenTheme, handleThemeChange, finalTheme } = useMyTheme();
@@ -50,3 +50,39 @@ export default function App({ Component, pageProps }) {
     </main>
   );
 }
+
+
+const useMyTheme = () => {
+  const [systemTheme, setSystemTheme] = useState("dark");
+  const [chosenTheme, setChosenTheme] = useState("system");
+
+  useEffect(() => {
+    detectTheme();
+  }, []);
+
+  const detectTheme = () => {
+    if (typeof window !== "undefined") {
+      const isDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setSystemTheme(isDarkMode ? "dark" : "light");
+
+      const userTheme = localStorage.getItem("chosenTheme");
+      setChosenTheme(userTheme !== null ? userTheme : "system");
+    }
+  };
+
+  const handleThemeChange = (theme) => {
+    setChosenTheme(theme);
+    localStorage.setItem("chosenTheme", theme);
+  };
+  const finalTheme =
+    chosenTheme === "system"
+      ? systemTheme === "dark"
+        ? darkTheme
+        : lightTheme
+      : chosenTheme === "dark"
+      ? darkTheme
+      : lightTheme;
+  return { chosenTheme, handleThemeChange, finalTheme };
+};
